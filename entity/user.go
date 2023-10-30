a292865ed3b93fdb2709f344c4bddcb7eb40e70c
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-10-31 00:34:13 +0800

    fix: user models

--- entity/user.go
+++ entity/user.go
@@ -6,8 +6,8 @@ type User struct {
 	ID        uint   `gorm:"primaryKey"`
 	FullName  string `gorm:"type:varchar(100);not null"`
 	Email     string `gorm:"type:varchar(100);not null;unique"`
-	Password  string
-	Role      string
+	Password  string `gorm:"type:varchar(100);not null"`
+	Role      string `gorm:"type:varchar(100);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
