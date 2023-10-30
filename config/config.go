95beaf25c0dd83e705e645d4f71297b656a7ef6e
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-10-31 00:10:07 +0800

    feat: make connection to db

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type appConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func LoadAppConfig() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+}
+
+func GetConfig() appConfig {
+	fmt.Println(os.Getenv("DB_HOST"))
+	return appConfig{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+	}
+}
---
+++ infra/database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	// "fmt"
+	// "log"
+
+	"fmt"
+
+	"github.com/hacktiv8-ks07-g04/final-project-3/config"
+	"github.com/hacktiv8-ks07-g04/final-project-3/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func ConnectDB() {
+	config := config.GetConfig()
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		config.DBHost,
+		config.DBUser,
+		config.DBPassword,
+		config.DBName,
+		config.DBPort,
+	)
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.Debug().AutoMigrate(entity.User{})
+}
