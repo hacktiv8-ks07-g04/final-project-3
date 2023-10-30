615aa770ef2f2b8fd0aa074c7f4c0ae00cf8c1d0
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-10-31 00:11:21 +0800

    feat: load env and connect to db

--- handler/app.go
+++ handler/app.go
@@ -2,9 +2,15 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hacktiv8-ks07-g04/final-project-3/config"
+	"github.com/hacktiv8-ks07-g04/final-project-3/infra/database"
 )
 
 func StartApp() {
+	config.LoadAppConfig()
+
+	database.ConnectDB()
+
 	r := gin.Default()
 	ping := func(c *gin.Context) {
 		c.JSON(200, gin.H{
