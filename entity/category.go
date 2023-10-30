d0c85e384f6d59c4b874b09922acfadc14ff8d1b
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-10-31 00:34:32 +0800

    feat: add category models

---
+++ entity/category.go
@@ -0,0 +1,10 @@
+package entity
+
+import "time"
+
+type Category struct {
+	ID        uint   `gorm:"primaryKey;not null"`
+	Type      string `gorm:"type:varchar(255);not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
