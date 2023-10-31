4abbc8cac411f61d9025d17cbd8e0efc0dca5d3a
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-10-31 16:15:41 +0800

    feat: add user dto

---
+++ dto/user.go
@@ -0,0 +1,22 @@
+package dto
+
+import "time"
+
+type RegisterRequest struct {
+	FullName string `json:"full_name"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type UserDataResponse struct {
+	ID        int       `json:"id"`
+	FullName  string    `json:"full_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type RegisterResponse struct {
+	StatusCode int              `json:"status"`
+	Message    string           `json:"message"`
+	Data       UserDataResponse `json:"data"`
+}
