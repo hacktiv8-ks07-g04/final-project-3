7e471413c9af6f658469b2e7ae3735440944aca0
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-10-31 22:31:18 +0800

    fix: add user entity as return value

--- repository/user_repository/repository.go
+++ repository/user_repository/repository.go
@@ -1,6 +1,8 @@
 package user_repository
 
-import "github.com/hacktiv8-ks07-g04/final-project-3/entity"
+import (
+	"github.com/hacktiv8-ks07-g04/final-project-3/entity"
+)
 
 type Repository interface {
 	RegisterNewUser(user entity.User) (entity.User, error)
